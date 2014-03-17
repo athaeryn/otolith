fb19701e1ef923d6ad976cfd340c31c2b97e1aea
Mike Anderson <hello@mrmikea.com>
2014-03-17 01:11:12 -0500

    Improve documentation and options

--- README.md
+++ README.md
@@ -1,15 +1,83 @@
-# gimbal.js
+# Gimbal.js
 
-Gimbal.js helps you move things as you tilt your device. Tell it which axis you
-want to listen to, give it a callback, and watch it go!
+Gimbal.js lets you control things by tilting your device.
 
-Note: you should not set up Gimbal if `window.DeviceOrientation` is undefined.
 
-## Use it
+## Usage
+
+To use Gimbal.js, tell it which axis you want to listen to, give it a
+callback, and watch it go! Gimbal.js only allows rotation on one axis, just
+like a [real gimbal](http://en.wikipedia.org/wiki/Gimbal), but there's nothing
+stopping you setting up more than one.
 
 ```js
-var gimbal = new Gimbal({
-  axis: 'x',
-  handler: gimbalHandler
+if (window.DeviceOrientationEvent) {
+  gimbal = new Gimbal({
+    axis: 'x',
+    callback: function (val) {
+      console.log(val);
+    }
+  );
+}
+```
+
+The value passed to the callback is a number in the range [-1,1] representing
+the current point through the rotation.
+
+If you wish, you may stop your callback being called, either temporarily or
+permanently. You can "pause" the gimbal with the `lock` function, and "unpause"
+it with the `unlock` function. Don't worry, [gimbal
+lock](http://en.wikipedia.org/wiki/Gimbal_lock) isn't an issue.
+
+
+## Options
+
+### axis
+
+__(required)__
+
+The axis you want to watch. Can be `'x'`, `'y'`, or `'z'`.
+
+
+### callback
+
+__(required)__
+
+Will be called a lot. Be prepared.
+
+
+### range
+
+_(optional)_
+
+```js
+gimbal = new Gimbal({
+  axis: 'y',
+  callback: gimbalCallback,
+  range: 35
 });
 ```
+
+This will map the extremes of the value passed to your callback to a smaller
+range of rotation. For instance, if you pass a range of 35, any degree of
+rotation greater than 35&deg; will produce a value of 1. This works in the
+negative direction as well.
+
+
+### raw
+
+_(optional)_
+
+If you'd rather receive the raw data from the event, you can pass the `raw`
+option:
+
+```js
+gimbal = new Gimbal({
+  axis: 'y',
+  callback: gimbalCallback,
+  raw: true
+});
+```
+
+When this option is set, the value given to the callback will be a number in
+the range [-180,180].
--- gimbal.js
+++ gimbal.js
@@ -1,40 +1,55 @@
 /*jslint browser: true */
 
-(function() {
+(function(global) {
   "use strict";
-  var _bind = function(func, context) {
+  var _bind, _clamp;
+
+  function Gimbal(options) {
+    this._options = options || {};
+    this._options.range = this._options.range || 180;
+    this._options.raw = this._options.raw || false;
+
+    // Look up the axis to use.
+    this._axis = { x: 'gamma', y: 'beta', z: 'alpha' }[this._options.axis];
+
+    // Ensure proper context is maintained.
+    this.lock = _bind(this.lock, this);
+    this.unlock = _bind(this.unlock, this);
+    this._calculateValue = _bind(this._calculateValue, this);
+    this._handleOrientationChange = _bind(this._handleOrientationChange, this);
+
+    // Start the event listening.
+    this.unlock();
+  }
+
+
+  Gimbal.prototype = {
+    lock: function() {
+      global.removeEventListener('deviceorientation', this._handleOrientationChange, false);
+    },
+    unlock: function() {
+      global.addEventListener('deviceorientation', this._handleOrientationChange, false);
+    },
+    _handleOrientationChange: function(e) {
+      var raw = e[this._axis];
+      this._options.callback(
+        this._options.raw ? raw : this._calculateValue(raw)
+      );
+    },
+    _calculateValue: function(raw) {
+      return _clamp((raw / this._options.range), -1, 1);
+    }
+  };
+
+
+  _bind = function(func, context) {
     return function() {
       return func.apply(context, arguments);
     };
   };
-
-
-  function Gimbal(options) {
-    this.options = options;
-    this.options.range = this.options.range || 180;
-    this._handleOrientationChange = _bind(this._handleOrientationChange, this);
-    this._calculatePercent = _bind(this._calculatePercent, this);
-    this.axes = {
-      x: 'gamma',
-      y: 'beta',
-      z: 'alpha'
-    };
-
-    window.addEventListener('deviceorientation', this._handleOrientationChange, false);
-  }
-
-  Gimbal.prototype._handleOrientationChange = function(e) {
-    var percent = this._calculatePercent(e[this.axes[this.options.axis]]);
-    this.options.callback(percent);
+  _clamp = function(val, min, max) {
+    return Math.max(min, Math.min(max, val));
   };
 
-  Gimbal.prototype._calculatePercent = function(raw) {
-    return Gimbal._bounds(raw / this.options.range * 100);
-  };
-
-  Gimbal._bounds = function(percent) {
-    return Math.floor(Math.max(-100, Math.min(100, percent)));
-  };
-
-  window.Gimbal = Gimbal;
-}());
+  global.Gimbal = Gimbal;
+}(window));
